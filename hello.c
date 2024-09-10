0cde5a49edec164cd3b160b30452103ccce43b8c
virtp <virt.p.q@yandex.ru>
2024-09-10 22:16:28 +0300

    first commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #include <Windows.h>
+// #include <locale.h>
+
+int main(void)
+{
+    // setlocale(LC_ALL, "Rus");
+    // SetConsoleCP(1251);
+    // SetConsoleOutputCP(1251);
+
+    printf("Привет, мир! Наконец-то я тут");
+    return 0;
+}
